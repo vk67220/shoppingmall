057d21c8044edc06c65e3b6cacbf62d4a22328ce
Murali Nutalapati <info@muraliprashanth.me>
2018-10-21 13:47:03 +0530

    resource created at /api/customers

--- src/main/java/com/shoppingmall/controllers/CustomersController.java
+++ src/main/java/com/shoppingmall/controllers/CustomersController.java
@@ -1,4 +1,21 @@
 package com.shoppingmall.controllers;
 
+import com.shoppingmall.entity.Customer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author muralinutalapati
+ */
+@RestController
+@RequestMapping("/api")
 public class CustomersController {
+
+	@GetMapping("/customers")
+	public List<Customer> getCustomers() {
+		return null;
+	}
 }
