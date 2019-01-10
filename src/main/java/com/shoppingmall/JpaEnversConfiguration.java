a1febb73a360d6f1d683e6cb4db27f6f024c09f4
Murali Nutalapati <info@muraliprashanth.me>
2019-01-11 00:03:44 +0530

    enable scheduling to ping shopping mall application

--- src/main/java/com/shoppingmall/JpaEnversConfiguration.java
+++ src/main/java/com/shoppingmall/JpaEnversConfiguration.java
@@ -2,11 +2,14 @@ package com.shoppingmall;
 
 import com.shoppingmall.config.FileStorageProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableJpaAuditing
+@EnableScheduling
 @EnableConfigurationProperties(FileStorageProperties.class)
 public class JpaEnversConfiguration {
 }
--- src/main/java/com/shoppingmall/ShoppingmallApplication.java
+++ src/main/java/com/shoppingmall/ShoppingmallApplication.java
@@ -1,14 +1,26 @@
 package com.shoppingmall;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.LocalDateTime;
 
 /**
  * @author muraliprasanth.n
  */
 @SpringBootApplication
 public class ShoppingmallApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(ShoppingmallApplication.class, args);
-	}
+  Logger logger = LoggerFactory.getLogger(getClass().getName());
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShoppingmallApplication.class, args);
+  }
+
+  @Scheduled(initialDelay = 1000, fixedRate = 10000)
+  public void run() {
+    logger.info("logging shopping mall app at " + LocalDateTime.now());
+  }
 }
