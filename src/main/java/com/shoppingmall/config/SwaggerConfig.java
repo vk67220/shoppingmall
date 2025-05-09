3e15db26450e346efdd063eb6c74b4cb53b5902c
vk67220 <varaprasadkasturi.dba@gmail.com>
2025-05-09 18:21:00 +0530

    Update SwaggerConfig.java

--- src/main/java/com/shoppingmall/config/SwaggerConfig.java
+++ src/main/java/com/shoppingmall/config/SwaggerConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.info.Info;
 
+
+
 @Configuration
 public class SwaggerConfig {
 
