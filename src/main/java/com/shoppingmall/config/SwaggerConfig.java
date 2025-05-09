46c1304258e7b0ded6321bc826139aa637a09765
vk67220 <varaprasadkasturi.dba@gmail.com>
2025-05-09 19:30:41 +0530

    Update SwaggerConfig.java

--- src/main/java/com/shoppingmall/config/SwaggerConfig.java
+++ src/main/java/com/shoppingmall/config/SwaggerConfig.java
@@ -1,13 +1,9 @@
-
-
 package com.shoppingmall.config;
 
-import org.springdoc.core.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-
-
 
 @Configuration
 public class SwaggerConfig {
@@ -15,7 +11,9 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info().title("Shopping Mall API").version("1.0").description("API documentation for Shopping Mall"));
+                .info(new Info()
+                        .title("Shopping Mall API")
+                        .version("1.0")
+                        .description("API documentation for Shopping Mall"));
     }
 }
-
